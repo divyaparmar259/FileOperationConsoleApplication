f030f39bc6bdb0eef287fdef3d63655a53915e43
divyaparmar259 <divyaparmar259@gmail.com>
2020-03-30 15:32:12 +1400

    ConsoleApp Done

---
+++ FileOperationConsoleApp/File.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FileOperationConsoleApp
+{
+    public class File
+    {
+        public void CreateFile()
+        {
+            Console.WriteLine("Enter FileName:");
+            string fn = Console.ReadLine();
+            FileStream f = new FileStream(@"C:\Test\Files\" + fn + ".txt", FileMode.Create);
+            Console.WriteLine("File Created..");
+            f.Close();
+        }           
+        public void DisplayData()
+        {
+            Console.WriteLine("Enter FileName");
+            string fn = Console.ReadLine();
+            
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Test\Files\" + fn + ".txt");
+
+            System.Console.WriteLine("Contents of File = ");
+            foreach (string line in lines)
+            {
+                Console.WriteLine("\t" + line);
+            }
+
+        }
+
+        public void CopyData()
+        {
+            Console.WriteLine("Enter First FileName");
+            string fn1 = Console.ReadLine();
+            Console.WriteLine("Enter Second FileName");
+            string fn2 = Console.ReadLine();
+            string content = System.IO.File.ReadAllText(@"C:\Test\Files\" + fn1 + ".txt");
+            System.IO.File.AppendAllText(@"C:\Test\Files\" + fn2 + ".txt", content);
+            Console.WriteLine("Content Copied from OneFile to Another");
+
+        }
+        public void RenameFile()
+        {
+            Console.WriteLine("Enter File To Rename");
+            string fn1 = Console.ReadLine();
+            Console.WriteLine("Enter New File Name");
+            string fn2 = Console.ReadLine();
+            string sourceFile = @"C:\Test\Files\" + fn1 + ".txt";
+            FileInfo fi = new FileInfo(sourceFile);
+            if (fi.Exists)
+            {
+                
+                fi.MoveTo(@"C:\Test\Files\" + fn2 + ".txt");
+                Console.WriteLine("File Renamed.");
+            }
+        }
+        public void ConcateFile()
+        {
+            Console.WriteLine("Enter First FileName");
+            string fn1 = Console.ReadLine();
+            Console.WriteLine("Enter Second FileName");
+            string fn2 = Console.ReadLine();
+            Console.WriteLine("Enter Target FileName");
+            string fn3 = Console.ReadLine();
+            System.IO.File.WriteAllText((@"C:\Test\Files\" + fn3 + ".txt"), System.IO.File.ReadAllText(@"C:\Test\Files\" + fn1 + ".txt") + System.IO.File.ReadAllText(@"C:\Test\Files\" + fn2 + ".txt"));
+            Console.WriteLine("Content Copied to target File..");
+
+        }
+
+    }
+}
---
+++ FileOperationConsoleApp/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace FileOperationConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File file = new File();
+        repeat:
+            Console.WriteLine("File Operation");
+            Console.WriteLine("Choose Any One:");
+            Console.WriteLine("1. Create File");
+            Console.WriteLine("2. Display Content of File");
+            Console.WriteLine("3. Copy File");
+            Console.WriteLine("4. Rename File");
+            Console.WriteLine("5. Concate File");
+            Console.WriteLine("6. Exit");
+
+            int choice = Int32.Parse(Console.ReadLine());
+            switch(choice)
+            {
+                case 1:
+                    file.CreateFile();
+                    goto repeat;
+
+                case 2:
+                    file.DisplayData();
+                    goto repeat;
+
+                case 3:
+                    file.CopyData();
+                    goto repeat;
+
+                case 4:
+                    file.RenameFile();
+                    goto repeat;
+
+                case 5:
+                    file.ConcateFile();
+                    goto repeat;
+
+                case 6:
+                    break;
+
+                default:
+                    Console.WriteLine("Choose from the above only");
+                    goto repeat;
+
+
+
+            }
+        }
+    }
+}
